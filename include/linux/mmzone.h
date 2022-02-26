0d0c22a0a691648bd680a5dca85c8f81c9bcdeb8
Yu Zhao <yuzhao@google.com>
2022-02-25 20:31:52 -0700

    FROMLIST: mm: multi-gen LRU: exploit locality in rmap
    Searching the rmap for PTEs mapping each page on an LRU list (to test
    and clear the accessed bit) can be expensive because pages from
    different VMAs (PA space) are not cache friendly to the rmap (VA
    space). For workloads mostly using mapped pages, the rmap has a high
    CPU cost in the reclaim path.
    
    This patch exploits spatial locality to reduce the trips into the
    rmap. When shrink_page_list() walks the rmap and finds a young PTE, a
    new function lru_gen_look_around() scans at most BITS_PER_LONG-1
    adjacent PTEs. On finding another young PTE, it clears the accessed
    bit and updates the gen counter of the page mapped by this PTE to
    (max_seq%MAX_NR_GENS)+1.
    
    Server benchmark results:
      Single workload:
        fio (buffered I/O): no change
    
      Single workload:
        memcached (anon): +[4, 6]%
                             Ops/sec      KB/sec
          patch1-6:          964656.80    37520.88
          patch1-7:          1014393.57   39455.42
    
      Configurations:
        no change
    
    Client benchmark results:
      kswapd profiles:
        patch1-6
          36.13%  lzo1x_1_do_compress (real work)
          19.16%  page_vma_mapped_walk
           6.55%  _raw_spin_unlock_irq
           4.02%  do_raw_spin_lock
           2.32%  anon_vma_interval_tree_iter_first
           2.11%  ptep_clear_flush
           1.76%  __zram_bvec_write
           1.64%  folio_referenced_one
           1.40%  memmove
           1.35%  obj_malloc
    
        patch1-7
          45.54%  lzo1x_1_do_compress (real work)
           9.56%  page_vma_mapped_walk
           6.70%  _raw_spin_unlock_irq
           2.78%  ptep_clear_flush
           2.47%  do_raw_spin_lock
           2.22%  __zram_bvec_write
           1.87%  lru_gen_look_around
           1.78%  memmove
           1.77%  obj_malloc
           1.44%  free_unref_page_list
    
      Configurations:
        no change
    Moto-CRs-Fixed:(CR)
    
    Link: https://lore.kernel.org/r/20220309021230.721028-8-yuzhao@google.com/
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Acked-by: Brian Geffon <bgeffon@google.com>
    Acked-by: Jan Alexander Steffens (heftig) <heftig@archlinux.org>
    Acked-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Acked-by: Steven Barrett <steven@liquorix.net>
    Acked-by: Suleiman Souhlal <suleiman@google.com>
    Tested-by: Daniel Byrne <djbyrne@mtu.edu>
    Tested-by: Donald Carr <d@chaos-reins.com>
    Tested-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    Tested-by: Shuang Zhai <szhai2@cs.rochester.edu>
    Tested-by: Sofia Trinh <sofia.trinh@edi.works>
    Tested-by: Vaibhav Jain <vaibhav@linux.ibm.com>
    Bug: 228114874
    Change-Id: I9a290343840f3cf925c891c8e360c7cdc24ffb9c
    Reviewed-on: https://gerrit.mot.com/2268699
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -279,6 +279,7 @@ struct zone_reclaim_stat {
 
 struct lruvec;
 struct mem_cgroup;
+struct page_vma_mapped_walk;
 
 #define LRU_GEN_MASK		((BIT(LRU_GEN_WIDTH) - 1) << LRU_GEN_PGOFF)
 #define LRU_REFS_MASK		((BIT(LRU_REFS_WIDTH) - 1) << LRU_REFS_PGOFF)
@@ -312,6 +313,9 @@ struct lru_gen_struct {
 };
 
 void lru_gen_init_lruvec(struct lruvec *lruvec);
+void *lru_gen_eviction(struct page *page);
+void lru_gen_refault(struct page *page, void *shadow);
+void lru_gen_look_around(struct page_vma_mapped_walk *pvmw);
 
 #ifdef CONFIG_MEMCG
 void lru_gen_init_memcg(struct mem_cgroup *memcg);
@@ -321,6 +325,19 @@ void lru_gen_exit_memcg(struct mem_cgroup *memcg);
 #else /* !CONFIG_LRU_GEN */
 
 static inline void lru_gen_init_lruvec(struct lruvec *lruvec)
+{
+}
+
+static inline void *lru_gen_eviction(struct page *page)
+{
+	return NULL;
+}
+
+static inline void lru_gen_refault(struct page *page, void *shadow)
+{
+}
+
+static inline void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 {
 }
 
